392b3ef8a2437a2eec30a718d525a61b753a3ba7
Miguel Montania <mikemontania@gmail.com>
2023-11-16 18:42:31 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GuestService } from './services/guest.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tienda';
+  public geo = localStorage.getItem('geo');
+
+
+  constructor(
+    private _guestService:GuestService,
+    private _router:Router
+  ){
+    if(localStorage.getItem('token') != null){
+      this._guestService.verificar_token(localStorage.getItem('token')).subscribe(
+        response=>{
+        },
+        error=>{
+          localStorage.removeItem('token');
+          localStorage.removeItem('id');
+          localStorage.removeItem('user');
+          this._router.navigate(['/login']);
+        }
+      );
+    }
+    if(this.geo == null){
+      this._guestService.obtener_ip_cliente().subscribe(
+        response=>{
+          this._guestService.obtener_data_cliente(response.ip).subscribe(
+            response=>{
+              localStorage.setItem('geo',JSON.stringify(response));
+              window.location.reload();
+
+            }
+          );
+        }
+      );
+    }
+
+
+  }
+
+  onActivate(event:any) {
+    window.scroll(0,0);
+
+  }
 }
---
+++ src/app/services/GLOBA.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+  url: 'http://127.0.0.1:8080/api/',
+  socket: 'http://localhost:8080'
+}
